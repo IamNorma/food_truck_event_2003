a7d2c7c5e57ffb57b39941cdabf12b36f47b3719
Norma Lopez <norma_l304@yahoo.com>
2020-04-15 10:35:27 -0600

    Add test for stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -22,10 +22,18 @@ class FoodTruckTest < Minitest::Test
     assert_equal ({}), food_truck.inventory
   end
 
-  def test_it_can_check_stock
+  def test_stock_starts_at_zero
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    
+
     assert_equal 0, food_truck.check_stock(item1)
+  end
+
+  def test_it_can_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+
+    assert_equal ({item1 => 30}), food_truck.inventory
   end
 end
