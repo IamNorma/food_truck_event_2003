84a86f92bc6bc349819e2fcffcb5bac393bc52e8
Norma Lopez <norma_l304@yahoo.com>
2020-04-15 10:29:18 -0600

    Add method for check stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,8 +1,13 @@
 class FoodTruck
-  attr_reader :name, :inventory
+  attr_reader :name, :inventory, :check_stock
 
   def initialize(name)
     @name = name
     @inventory = {}
+    @check_stock = 0
+  end
+
+  def check_stock(item)
+    @check_stock
   end
 end
